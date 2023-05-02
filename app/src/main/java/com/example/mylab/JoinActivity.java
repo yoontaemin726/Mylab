8e8680bcf07659078c101d52a1ceefca734297cc
yoontaemin726 <2003taemin@gmail.com>
2023-05-02 22:25:54 +0900

    simple code

--- app/src/main/java/com/example/mylab/JoinActivity.java
+++ app/src/main/java/com/example/mylab/JoinActivity.java
@@ -30,7 +30,7 @@ public class JoinActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
-        super. onCreate (savedInstanceState);
+        super.onCreate (savedInstanceState);
         setContentView(R.layout.activity_join);
 
 
